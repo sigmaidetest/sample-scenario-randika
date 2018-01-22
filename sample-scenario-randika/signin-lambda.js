0c54c11b79882a53a5cb789b26dc5746480838b8
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-22 13:59:34 +0530

    Adding signin lambda

---
+++ sample-scenario-randika/signin-lambda.js
@@ -0,0 +1,30 @@
+let AWS = require('aws-sdk');
+let connectionManager = require('./ConnectionManager');
+let SL = require('@slappforge/slappforge-sdk');
+const rds = new SL.AWS.RDS(connectionManager);
+exports.handler = function (event, context, callback) {
+
+    let results;
+	// Replace the query with the actual query
+	// You can pass the existing connection to this function.
+	// A new connection will be created if it's not present as the third param 
+	// You must always end the DB connection after it's used
+	rds.query({
+		instanceIdentifier: 'mobilebackend',
+		query: 'SELECT * FROM users WHERE email = ? and password = ?',
+		inserts: ["randikanavagamuwa@gmail.com", "password"]
+	}, function (error, results, connection) {
+		if (error) {
+			results = results;
+			throw error;
+		} else {
+			results = "Failed to process"
+			console.log(results);
+		}
+
+		connection.end();
+	});
+
+
+	callback(null, results);
+}
