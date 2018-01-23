c4d5f14d64b45a4b32565cb0017e4cf4ba1de439
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-23 12:00:33 +0530

    minor fix

--- sample-scenario-randika/signin-lambda.js
+++ sample-scenario-randika/signin-lambda.js
@@ -4,7 +4,7 @@ let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 
-    let results;
+    let response;
 	// Replace the query with the actual query
 	// You can pass the existing connection to this function.
 	// A new connection will be created if it's not present as the third param 
@@ -12,19 +12,19 @@ exports.handler = function (event, context, callback) {
 	rds.query({
 		instanceIdentifier: 'mobileBackend',
 		query: 'SELECT * FROM users',
-		inserts: ["randikanavagamuwa@gmail.com", "password"]
+		inserts: [event.email, event.password]
 	}, function (error, results, connection) {
 		if (error) {
-			results = error;
+			response = error;
 			throw error;
 		} else {
-			results = "Failed to process"
-			console.log(results);
+			response = results;
+			console.log(response);
 		}
 
 		connection.end();
 	});
 
 
-	callback(null, results);
+	callback(null, response);
 }
