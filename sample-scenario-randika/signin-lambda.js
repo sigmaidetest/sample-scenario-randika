41208b204dec8cae506341e48e8877f1109f6211
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-01-23 12:22:37 +0530

    Fixed signin api

--- sample-scenario-randika/signin-lambda.js
+++ sample-scenario-randika/signin-lambda.js
@@ -4,27 +4,24 @@ let SL = require('@slappforge/slappforge-sdk');
 const rds = new SL.AWS.RDS(connectionManager);
 exports.handler = function (event, context, callback) {
 
-    let response;
+    let successfullyLoggedIn = false;
 	// Replace the query with the actual query
 	// You can pass the existing connection to this function.
 	// A new connection will be created if it's not present as the third param 
 	// You must always end the DB connection after it's used
 	rds.query({
 		instanceIdentifier: 'mobileBackend',
-		query: 'SELECT * FROM users',
+		query: 'SELECT * FROM users WHERE Email = ? AND Password = ?',
 		inserts: [event.email, event.password]
 	}, function (error, results, connection) {
 		if (error) {
-			response = error;
 			throw error;
 		} else {
-			response = results;
-			console.log(response);
+			successfullyLoggedIn = results.length > 0;
 		}
 
 		connection.end();
+		callback(null, successfullyLoggedIn);
 	});
 
-
-	callback(null, response);
 }
